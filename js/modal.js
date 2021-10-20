1e4e9dc930b0f241fd59282e54b75f12b75310ec
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2021-10-21 00:48:26 +0200

    Remove modal and use WIP bar instead

--- js/modal.js
+++
@@ -1,6 +0,0 @@
-var close = document.getElementById('close');
-var modal = document.getElementById('modal');
-close.addEventListener('click', function (event) {
-  event.preventDefault();
-  modal.style.display = 'none';
-});
