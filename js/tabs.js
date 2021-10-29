b5936ee7a594b623a95ec32c6fda6efd0a9ede69
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2021-10-29 21:06:49 +0100

    Important comment about parent selectors and the navigation menu

--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,28 @@
-function f(t) {
-  const sections = document.getElementsByTagName('main')[0].getElementsByTagName('section')
-  const sections_array = [...sections];
-  sections_array.map(x => x.style.setProperty('display', 'none'));
-  if (document.getElementById(t)) {
-    document.getElementById(t).style.setProperty('display', 'initial');
+// This JavaScript is needed for the following reason.  The choice of the tab
+// happens via the `<nav>` element, which contains a `<input type="radio">`
+// corresponding to each tab (specifically a `<section>`). The tabs, on the
+// other hand, are not in `<nav>`, so there's no sibling nor parent-child(ren)
+// relation between the radio buttons to chose the tabs, and the tabs
+// themeselves, therefore [CSS has no way][1] to target the tabs based on some
+// pseudo-class of the radio buttons. If the browsers will start supporting
+// [Selectors Level 4 Working Draft][2], then maybe something like this could
+// be possible:
+// ```css
+// :root:has(nav input[value=posts]:checked) section[id=posts] {
+//   /* these rules apply to `<section id="posts">` when it is checked */
+// }
+// ```
+// However this also means I'd have to write a selector for each new tab that I
+// add, because I'm hardcoding the `posts` word in the selector, which is not
+// really nice.
+//
+// [1]: https://stackoverflow.com/a/1014958/5825294
+// [2]: https://dev.w3.org/csswg/selectors4/#relational
+function changeTab(idNewTab) {
+  const sections = [...document.getElementById('main').getElementsByTagName('section')];
+  const idx = sections.indexOf(document.getElementById(idNewTab));
+  sections.map(section => section.style.setProperty('display', 'none'));
+  if (idx != -1) {
+    sections[idx].style.setProperty('display', 'initial');
   }
 }
