3e3722b7b49ed1ee151b143d6a2b44729014ba57
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2021-10-30 10:57:51 +0100

    Camel case JS functions

--- js/stackoverflow.js
+++ js/stackoverflow.js
@@ -1,4 +1,4 @@
-function load_stackoverflow_info() {
+function loadStackoverflowInfo() {
   const url = 'https://api.stackexchange.com/2.3/users/5825294?order=desc&sort=reputation&site=stackoverflow';
   fetch(url)
   .then(response => response.text())
@@ -7,7 +7,7 @@ function load_stackoverflow_info() {
     if (items) {
       const reputation = items[0].reputation;
       const badge_counts = items[0].badge_counts;
-      document.getElementById('reputation').innerHTML = separate_thousands_with_comma(reputation);
+      document.getElementById('reputation').innerHTML = separateThousandsWithComma(reputation);
       document.getElementById('bronze-badges').innerHTML = badge_counts.bronze;
       document.getElementById('silver-badges').innerHTML = badge_counts.silver;
       document.getElementById('gold-badges').innerHTML = badge_counts.gold;
@@ -15,6 +15,6 @@ function load_stackoverflow_info() {
   });
 }
 
-function separate_thousands_with_comma(num) {
+function separateThousandsWithComma(num) {
   return num.toString().match(/(\d{1,3})(?=(\d{3})*$)/g).join();
 }
