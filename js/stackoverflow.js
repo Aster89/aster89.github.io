ed5fa926a5713d5ba7394fa3de02c551a4d65df4
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2021-11-06 18:40:39 +0000

    Use `localStorage` to store StackOverflow data
    This is to have a means to retrieve those values even when the `fetch`ing fails
    because of throttling.

--- js/stackoverflow.js
+++ js/stackoverflow.js
@@ -1,18 +1,27 @@
 function loadStackoverflowInfo() {
   const url = 'https://api.stackexchange.com/2.3/users/5825294?order=desc&sort=reputation&site=stackoverflow';
   fetch(url)
-  .then(response => response.text())
-  .then(responseBody => {
-    const items = JSON.parse(responseBody).items;
-    if (items) {
-      const reputation = items[0].reputation;
-      const badge_counts = items[0].badge_counts;
-      $("#reputation").html(separateThousandsWithComma(reputation));
-      $("#bronze-badges").html(badge_counts.bronze);
-      $("#silver-badges").html(badge_counts.silver);
-      $("#gold-badges").html(badge_counts.gold);
-    }
-  });
+    .then(response => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.text();
+      }
+    })
+    .then(responseBody => {
+      const items = JSON.parse(responseBody).items;
+      if (items) {
+        localStorage.setItem('stackoverflow', JSON.stringify(items));
+      }
+    })
+    .finally(() => {
+      const itemsStr = localStorage.getItem('stackoverflow');
+      if (itemsStr != null) {
+        const items = JSON.parse(itemsStr);
+        $("#reputation").html(separateThousandsWithComma(items[0].reputation));
+        $("#bronze-badges").html(items[0].badge_counts.bronze);
+        $("#silver-badges").html(items[0].badge_counts.silver);
+        $("#gold-badges").html(items[0].badge_counts.gold);
+      }
+    });
 }
 
 function separateThousandsWithComma(num) {
